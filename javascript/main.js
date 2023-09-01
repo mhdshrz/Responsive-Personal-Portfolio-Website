f1ab7d2439f5c8ad1f170b2abe08a8421976a7db
elmaximo <mahdielmaximo@gmail.com>
2023-09-02 01:52:09 +0330

    add show/hide feature of navigatin menu

---
+++ javascript/main.js
@@ -0,0 +1,38 @@
+// show menu
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+// validating if the constant value exists
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    })
+}
+
+// validating if the constant value exists
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== SHADOW HEADER ===============*/
+
+
+/*=============== EMAIL JS ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
