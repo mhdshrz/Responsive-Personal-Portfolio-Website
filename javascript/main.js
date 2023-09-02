0d1cd568780babe63266ebe1f57e8c00c5c7710d
elmaximo <mahdielmaximo@gmail.com>
2023-09-02 16:26:17 +0330

    work on home, add profile secion with images and socials, add header shadow using vanilla javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,38 +1,48 @@
-// show menu
-const navMenu = document.getElementById('nav-menu');
-const navToggle = document.getElementById('nav-toggle');
-const navClose = document.getElementById('nav-close');
+// ------------ show menu ------------ //
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
 // validating if the constant value exists
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-    })
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show__menu");
+  });
 }
 
 // validating if the constant value exists
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    })
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show__menu");
+  });
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+// ------------ remove menu mobile ------------ //
+const navLink = document.querySelectorAll(".nav__link");
 
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show__menu");
+};
 
-/*=============== SHADOW HEADER ===============*/
+navLink.forEach((item) => item.addEventListener("click", linkAction));
 
+// ------------ shadow header ------------ //
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("shadow__header")
+    : header.classList.remove("shadow__header");
+};
+
+window.addEventListener("scroll", shadowHeader);
 
 /*=============== EMAIL JS ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
