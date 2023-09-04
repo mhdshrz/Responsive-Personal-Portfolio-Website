02c2097c44ef6b525d00c54ee112250091be3b27
elmaximo <mahdielmaximo@gmail.com>
2023-09-04 22:43:32 +0330

    add scroll reveal animations

--- javascript/main.js
+++ javascript/main.js
@@ -144,3 +144,23 @@ themeButton.addEventListener("click", () => {
 });
 
 // -------------------- scroll reveal animation -------------------- //
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true
+});
+
+sr.reveal(
+  `.home__profile, .about__image, 
+    .contact__mail`,
+  { origin: "right" }
+);
+sr.reveal(
+  `.home__name, .home__info, .about__container 
+    .section__title__1, .about__info, .contact__social, 
+    .contact__data`,
+  { origin: "left" }
+);
+sr.reveal(`.services__card, .projects__card`, { interval: 100 });
